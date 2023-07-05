8e55afcf0893b3420db35162252d267fd9def892
Khang Tran <khangtrann8198@gmail.com>
2023-07-05 07:11:33 +0000

    Implement caLoad for admin module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationGuard } from './auth/authentication.guard';
+import { LoadGuard } from './auth/load.guard';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: 'admin',
     loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule),
-    canActivate: [AuthenticationGuard],
+    // If we use canActivate here, it means even user isn't authorized, the app module js will be still loaded
+    // canActivate: [AuthenticationGuard],
+    canLoad: [LoadGuard],
   },
   {
     path: '',
---
+++ src/app/auth/load.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanLoad, Route, Router, UrlSegment, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoadGuard {
+  constructor(private auth: AuthService, private router: Router) {}
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]
+  ):
+    | boolean
+    | UrlTree
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree> {
+    return this.auth
+      .isLoggedIn()
+      .pipe((isLoggedIn) => isLoggedIn || this.router.createUrlTree(['']));
+  }
+}
