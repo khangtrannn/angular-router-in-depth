8446c527a0f00120da0b3a76199581830d032e69
Khang Tran <khangtrann8198@gmail.com>
2023-07-06 00:56:43 +0700

    Fix named router details navigate issue

--- src/app/user/user-list.component.ts
+++ src/app/user/user-list.component.ts
@@ -14,7 +14,7 @@ import { UsersService } from './users.service';
         </div>
         <a
           mat-stroked-button
-          [routerLink]="['users', { outlets: { details: ['users', user.id] } }]"
+          [routerLink]="['/users', { outlets: { details: [user.id] } }]"
           >Details</a
         >
       </div>
